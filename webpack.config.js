201350297f9b9eef74839a9287009f3c90f084c5
Sidney Packer <sidp@knights.ucf.edu>
2017-09-30 17:44:14 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('public'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env']
+          }
+        }
+      },
+      { 
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
