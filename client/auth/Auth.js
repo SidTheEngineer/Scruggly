a09215f338415511973bd38108bfd7854bbdb5bf
Sidney Packer <sidp@knights.ucf.edu>
2017-10-01 13:42:35 -0400

    Add more auth methods

--- client/auth/Auth.js
+++ client/auth/Auth.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import history from '../utils/history';
 
 export default class Auth {
   constructor() {
@@ -10,6 +11,43 @@ export default class Auth {
       responseType: 'token id_token',
       scope: 'openid',
     });
+
+    this.login = this.login.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+  }
+
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        Auth.setSession(authResult);
+        history.replace('/');
+      } else if (err) {
+        history.replace('/');
+        console.log(err);
+      }
+    });
+  }
+
+  static setSession(authResult) {
+    // Set the time that the access token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/');
+  }
+
+  static logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    history.replace('/');
+  }
+
+  static isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
   }
 
   login() {
