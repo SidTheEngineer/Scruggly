b3bd1330488765464abb61d11ded19b99203e627
Sidney Packer <sidp@knights.ucf.edu>
2017-10-11 23:19:43 -0400

    remove backend

--- backend/db/bookshelf.js
+++
@@ -1,7 +0,0 @@
-const config = require('config');
-const knex = require('knex')({
-  client: 'pg',
-  connection: config.get('dbConfig.connection')
-});
-
-module.exports = require('bookshelf')(knex);
--- backend/routes/config.js
+++
@@ -1,7 +0,0 @@
-const rootRouter = require('./routers/root');
-const apiRouter = require('./routers/api');
-
-module.exports = (app) => {
-  app.use('/', rootRouter);
-  app.use('/api', apiRouter);
-};
